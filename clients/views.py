fafdff327fcb10a0647a1ec5357c0ad4b7de827d
alejandro gonzalez ruiz <xelaalex@gmail.com>
2023-03-30 21:29:19 -0600

    clients views complete

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import  HttpResponseRedirect
+from .forms import ConvenienceStoreForm , ProvidersForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+def ProvidersFormView(request):
+    if request.method=="POST":
+        form=ProvidersForm(request.POST)
+        if form.is_valid():
+            provider=form.save()
+            messages.add_message(request, messages.SUCCESS, 'Registro exitoso')
+            return HttpResponseRedirect(reverse_lazy('CatalogueFormView', args=[provider.id]))
+    else:
+        form=ProvidersForm()
+    return render(request, "CatalogueRegister.html", {'form':form})
+def ConvenienceStoreFormView(request):
+    if request.method=="POST":
+        form=ConvenienceStoreForm(request.POST)
+        if form.is_valid():
+            form.save()        
+            messages.add_message(request, messages.SUCCESS, 'Registro exitoso')
+            return HttpResponseRedirect(reverse_lazy('ConvenienceStoreHome', args=[]))
+    else:
+        form=ConvenienceStoreForm()
+    return render(request, "ConvenienceStoreRegister.html",{'form': form})
+

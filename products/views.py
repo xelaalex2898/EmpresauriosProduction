b09d40819aad70af205e6d4af66aa9abb737a0d5
alejandro gonzalez ruiz <xelaalex@gmail.com>
2023-04-04 21:13:40 -0600

    products views complete

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import  HttpResponseRedirect
+from .forms import CatalogueForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+def CatalogueFormView(request):
+    if request.method=="POST":
+        form=ProductsForm(request.POST)
+        if form.is_valid():
+            product=form.save()
+            messages.add_message(request, messages.SUCCESS, 'Registro exitoso')
+            return HttpResponseRedirect(reverse_lazy('CatalogueFormView', args=[provider.id]))
+    else:
+        form=ProductsForm()
+    return render(request, "CatalogueRegister.html", {'form':form})
